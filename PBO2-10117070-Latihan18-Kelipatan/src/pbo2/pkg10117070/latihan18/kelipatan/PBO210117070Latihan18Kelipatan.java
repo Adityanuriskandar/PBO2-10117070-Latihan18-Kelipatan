349e6f250d0154245c2362f75d34c9d0c57349c0
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-10-02 00:45:55 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan18-Kelipatan/src/pbo2/pkg10117070/latihan18/kelipatan/PBO210117070Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan18.kelipatan;
+
+/**
+ *
+ * @author Aditya
+ * Nama                 : Aditya Nur Iskandar
+ * Kelas                : PBO2/IF2
+ * NIM                  : 10117070
+ * Deskripsi Program    : Program yang berisi tentang mengetahui coding 
+ * Kelipatan.
+ */
+public class PBO210117070Latihan18Kelipatan {
+    public static void main(String[] args) {
+       double k = 3.5;
+        do {
+            System.out.println(k);
+            
+            k = k + 3.5;
+            
+        }while (k <= 35.0);
+    }
+    
+}
